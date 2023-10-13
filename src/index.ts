52abe14fedd9a0a382e9caaf1ac571e9006517f2
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2023-10-13 22:00:17 +0400

    added basic folders and basic packages

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", function (req, res) {
-  res.send("Hello World");
+  res.send({ data: '"Hello World"' });
 });
 
 app.listen(PORT, () => {
