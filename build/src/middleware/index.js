a63ebc618f9109cb728b7d4f76e35856473ff794
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2023-10-17 20:59:00 +0400

    Remove ignored files

--- build/src/middleware/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.logs = void 0;
-const logs_1 = __importDefault(require("./logs"));
-exports.logs = logs_1.default;
