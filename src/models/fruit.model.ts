4102a045668689c2607f48e820c3d9784d1f0ff5
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2023-10-22 15:49:42 +0400

    added pre and post save methods

--- src/models/fruit.model.ts
+++ src/models/fruit.model.ts
@@ -1,5 +1,23 @@
-import { prop, getModelForClass, ReturnModelType } from "@typegoose/typegoose";
+import {
+  prop,
+  post,
+  pre,
+  getModelForClass,
+  ReturnModelType,
+} from "@typegoose/typegoose";
 
+/**
+ * This will execute before the save call execute
+ */ @pre<Fruit>("save", function () {
+  this.isLowPrice = this.price < 5;
+})
+/**
+ * This will execute after the save call done
+ */
+@post<Fruit>("save", function (fruit) {
+  console.log(fruit);
+  console.log("We have a new fruit here.");
+})
 export class Fruit {
   @prop({ required: true })
   public name!: string;
@@ -9,6 +27,9 @@ export class Fruit {
 
   @prop({ required: false, default: true })
   public isOutOfStock?: boolean;
+
+  @prop({ required: false, default: false })
+  public isLowPrice?: boolean;
 
   // the "this" definition is required to have the correct types
   public static async findByFruitName(
